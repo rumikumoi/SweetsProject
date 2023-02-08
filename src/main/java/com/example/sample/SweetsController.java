2842b26d0623f5a89ac4d62135c95431290213c1
kumoi <rumi.kumoi@azone-japan.co.jp>
2023-02-08 20:36:10 +0900

    課題５デザイン

--- src/main/java/com/example/sample/SweetsController.java
+++ src/main/java/com/example/sample/SweetsController.java
@@ -24,83 +24,35 @@ public class SweetsController {
 		return "showcase";
 	}
 
+//	@RequestMapping("/shop")
+//	public String shop(Model model) {
+//		model.addAttribute("sweetsForm", service.getShopData());
+//		return "shop";
+//	}
+//
+//	@PostMapping("/buy")
+//	public String buy(SweetsForm sweetsForm, Model model) {
+//		model.addAttribute("sweets", service.updateStock(sweetsForm));
+//		return "thanks";
+//	}
+
 	@RequestMapping("/shop")
-	public String shop(Model model) {
-		model.addAttribute("sweetsForm", service.getShopData());
+	public String shop2(Model model) {
+		model.addAttribute("sweetsForm", service.getShopData2());
 		return "shop";
 	}
 
 	@PostMapping("/buy")
-	public String buy(SweetsForm sweetsForm, Model model) {
-		model.addAttribute("sweets", service.updateStock(sweetsForm));
-		return "thanks";
-	}
-
-	@RequestMapping("/shop2")
-	public String shop2(Model model) {
-		model.addAttribute("sweetsForm", service.getShopData2());
-		return "shop2";
-	}
-
-	@PostMapping("/buy2")
 	public String buy2(SweetsForm sweetsForm, Model model) {
 		List<SweetsData> list = service.updateStock2(sweetsForm);
-//		model.addAttribute("sweets", list);
-//		return "thanks2";
 		if (list.size() > 0) {
 			model.addAttribute("sweets", list);
-			return "thanks2";
+			return "thanks";
 		} else {
 			model.addAttribute("sweetsForm", sweetsForm);
 			model.addAttribute("msg", "商品を選んでください");
-			return "shop2";
+			return "shop";
 		}
 	}
-//
-//	@Autowired
-//	BackyardService backyardService;
-//	
-//	@RequestMapping("/backyard")
-//	public String backyard(Model model) {
-//		model.addAttribute("sweetsList", backyardService.getSweetsList());
-//		BackyardForm form = new BackyardForm();
-//		model.addAttribute("backyardForm", form);
-//		return "backyard";
-//	}
-//
-//	@PostMapping("/stock-add")
-//	public String stockAdd(BackyardForm form, Model model) {
-//		model.addAttribute("msg1", backyardService.addStock(form.getId(), form.getAddStock()));
-//
-//		// 続けて更新ができるよう商品一覧とformを再送
-//		model.addAttribute("sweetsList", backyardService.getSweetsList());
-//		model.addAttribute("backyardForm", form);
-//		
-//		return "backyard";
-//	}
-//
-//	@PostMapping("/item-insert")
-//	public String itemInsert(Sweets sweets, Model model) {
-//		model.addAttribute("msg2", backyardService.insertItem(sweets));
-//
-//		// 続けて更新ができるよう商品一覧とformを再送
-//		model.addAttribute("sweetsList", backyardService.getSweetsList());
-//		BackyardForm form = new BackyardForm();
-//		model.addAttribute("backyardForm", form);
-//
-//		return "backyard";
-//	}
-//
-//	@PostMapping("/item-delete")
-//	public String itemDelete(String id, Model model) {
-//		model.addAttribute("msg3", backyardService.deleatItem(id));
-//
-//		// 続けて更新ができるよう商品一覧とformを再送
-//		model.addAttribute("sweetsList", backyardService.getSweetsList());
-//		BackyardForm form = new BackyardForm();
-//		model.addAttribute("backyardForm", form);
-//
-//		return "backyard";
-//	}
 
 }
