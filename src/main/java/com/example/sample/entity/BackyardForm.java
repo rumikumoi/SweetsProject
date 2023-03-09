5b37fdbf41cf5b34cbc43ce76221c77c3ce8f0dd
matsui <az_training2@azone-japan.co.jp>
2023-03-10 00:19:48 +0900

    3/9時点での変更

--- src/main/java/com/example/sample/entity/BackyardForm.java
+++ src/main/java/com/example/sample/entity/BackyardForm.java
@@ -3,9 +3,7 @@ package com.example.sample.entity;
 public class BackyardForm extends Sweets{
 
 	private int addStock;
-	private String id;
 	private String newItem;
-	private String kind;
 	
 	public void setAddStock(int addStock) {
 		this.addStock = addStock;
@@ -15,27 +13,11 @@ public class BackyardForm extends Sweets{
 		return addStock;
 	}
 	
-	public void setId(String id) {
-		this.id = id;
-	}
-	
-	public String getId() {
-		return id;
-	}
-	
 	public void setNewItem(String newItem) {
 		this.newItem = newItem;
 	}
 	
 	public String getNewItem() {
 		return newItem;
-	}
-	
-	public void setKind(String kind) {
-		this.kind = kind;
-	}
-	
-	public String getKind() {
-		return kind;
 	}
 }
